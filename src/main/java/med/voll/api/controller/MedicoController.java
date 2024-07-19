bdc22e88324576c9a6448a1403f91422f7fc9818
Matheus Esteves <matheus.mat160805@gmail.com>
2024-07-19 13:07:06 -0300

    implementação do crud completo para médicos

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,47 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.medico.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository;
+
+    @PostMapping
+    @Transactional //anotação para quando ocorre alguma alteração no database
+    public void cadastrarMedico (@RequestBody @Valid DadosCadastroMedico dados){
+        repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listarMedico(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    } //url para o Pageable -> /medicos?size=10&page=1(pag desejada) //para ordenar adicionar sort=nome&
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+        var medico = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluirMedico(@PathVariable Long id){
+//        repository.deleteById(id); -> deletar em definitivo
+        var medico = repository.getReferenceById(id);
+        medico.inativar();
+
+    }
+
+
+}
